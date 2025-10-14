a87e046446d30e8524081b11241e7a6b866baffb
FELIPE BAIERSKI VIEIRA <felipe_b_vieira@estudante.sesisenai.org.br>
2025-10-14 10:09:13 -0300

    codigo para enviar palavras

--- S2 - Felipe Baierski/sketch_oct7b/Felipe/Felipe.ino
+++ S2 - Felipe Baierski/sketch_oct7b/Felipe/Felipe.ino
@@ -1,42 +1,54 @@
-#include <WiFi.h> 
+#include <WiFi.h>
 #include <PubSubClient.h>
-//nome do cliente wifi
+
+WiFiClient wifi_client;
+PubSubClient mqtt(wifi_client);
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
 const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
+const String topic = "felipe";
 
 const String brokerUser = "";
 const String brokerPass = "";
 
-WiFiClient wifi_client;
-
-PubSubClient mqtt(wifi_client);
-
-const String SSID = "FIESC_IOT_EDU"; //nome da rede wifi
-const String PASS = "8120gv08";//senha da rede
-
 void setup() {
   Serial.begin(115200);
   WiFi.begin(SSID, PASS);
-  Serial.println("Conectando no WiFi");
-  while(WiFi.status()!= WL_CONNECTED){
-     Serial.print(".");
-     delay(200);
+  Serial.println("Concetando no Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(200);
   }
   Serial.println("Conectado com sucesso!");
   mqtt.setServer(brokerURL.c_str(), brokerPort);
-String clientID = "S1-";
-clientID += String(random(0xffff), HEX);
-
-while(mqtt.connect(clientID.c_str()) == 0){
-  Serial.print(".");
-  delay(200);
-}
-Serial.println("\nConectado ao broker");
-
+  String clientID = "S4-";
+  clientID += String(random(0xffff), HEX);
+  while (mqtt.connect(clientID.c_str()) == 0) {
+    Serial.print(".");
+    delay(200);
+  }
+  mqtt.subscribe(topic.c_str());
+  mqtt.setCallback(callback);
+  Serial.println("\nConectado ao broker!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String mensagem = "";
+  if(Serial.available() > 0){
+    mensagem = Serial.readStringUntil('\n');
+    mensagem = "Felipe: " + mensagem;
+    mqtt.publish("mariah",mensagem.c_str());
+  }
+  mqtt.loop();
+}
 
+void callback(char* topic, byte* payload, unsigned long length){
+    String MensagemRecebida = "";
+    for(int i = 0; i < length; i++){
+      MensagemRecebida += (char) payload[i];
+    }
+    Serial.println(MensagemRecebida);
 }
