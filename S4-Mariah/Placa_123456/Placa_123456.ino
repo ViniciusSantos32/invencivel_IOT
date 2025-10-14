799686153681610e82b16ad0be2e818fa8e28dfe
mariahsoares <msmariahxy@gmail.com>
2025-10-14 10:16:19 -0300

    sbddb

--- S4-Mariah/Placa_123456/Placa_123456.ino
+++ S4-Mariah/Placa_123456/Placa_123456.ino
@@ -1,41 +1,54 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-WiFiClient wifi_client; //nome do cliente wifi
+WiFiClient wifi_client;
 PubSubClient mqtt(wifi_client);
 
-const String SSID = "FIESC_IOT_EDU"; //nome da rede wifi
-const String PASS = "8120gv08";//senha da rede
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
 
-const String brokerURL = "teste.mosquitto.org";
+const String brokerURL = "test.mosquitto.org";
 const int brokerPort = 1883;
+const String topic = "mariah";
 
 const String brokerUser = "";
 const String brokerPass = "";
 
 void setup() {
-  Serial.begin(115200); //configura a placa para mostrar na tela
-  WiFi.begin(SSID, PASS); //tenta conectar na rede
-  Serial.println("Conectando no WiFi");
-  while (WiFi.status()!= WL_CONNECTED){
-     Serial.print(".");
-     delay(200);
-  }
-  Serial.println("Conectado com sucesso!");
-  mqtt.setServer(brokerURL.c_str(),brokerPort);
-  String clientID = "S1-";
-  clientID += String(random(0xffff),HEX);
-  Serial.println("Conectado ao Broker");
-  while(mqtt.connect(clientID.c_str()) == 0){
+  Serial.begin(115200);
+  WiFi.begin(SSID, PASS);
+  Serial.println("Concetando no Wifi");
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado ao Broker!");
+  Serial.println("Conectado com sucesso!");
+  mqtt.setServer(brokerURL.c_str(), brokerPort);
+  String clientID = "S4-";
+  clientID += String(random(0xffff), HEX);
+  while (mqtt.connect(clientID.c_str()) == 0) {
+    Serial.print(".");
+    delay(200);
+  }
+  mqtt.subscribe(topic.c_str());
+  mqtt.setCallback(callback);
+  Serial.println("\nConectado ao broker!");
 }
 
-
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  String mensagem = "";
+  if(Serial.available() > 0){
+    mensagem = Serial.readStringUntil('\n');
+    mensagem = "mariah: " + mensagem;
+    mqtt.publish("vinicius",mensagem.c_str());
+  }
+  mqtt.loop();
+}
 
+void callback(char* topic, byte* payload, unsigned long length){
+    String MensagemRecebida = "";
+    for(int i = 0; i < length; i++){
+      MensagemRecebida += (char) payload[i];
+    }
+    Serial.println(MensagemRecebida);
 }
